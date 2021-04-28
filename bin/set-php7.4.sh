5edf4feb4173f83ade2ac6d35f118b1f0ce0d779
Florian Kwakkenbos <f.kwakkenbos@flooris.nl>
2021-04-28 17:12:07 +0200

    Bash scripts for setting default PHP version

---
+++ bin/set-php7.4.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm /usr/local/bin/php
+ln -s /usr/local/bin/php7.4 /usr/local/bin/php
---
+++ bin/set-php8.0.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm /usr/local/bin/php
+ln -s /usr/local/bin/php8.0 /usr/local/bin/php
